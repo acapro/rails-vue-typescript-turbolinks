4ca25f59e7ff683b45dffe8b944b7d8379a18a3a
Adam Cooper <mail@adamcooper.de>
2017-12-18 15:32:34 +0100

    adds jquery for ajax requests

--- app/javascript/components/app.vue
+++ app/javascript/components/app.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <h1>{{ title }}</h1>
-    <ul v-for="item in appointments">
+    <ul v-for="(item, i) in theAppointments"
+        :key="i">
       <li>
         {{item.petName}}
       </li>
@@ -10,23 +11,24 @@
 </template>
 
 <script lang='ts'>
-var apts = [{"petName":"Spot","petOwner":"Constance Smith","aptDate":"2017-07-24 08:30","aptNotes":"This German Shepherd is having some back pain"},{"petName":"Goldie","petOwner":"Barot Bellingham","aptDate":"2017-07-22 15:50","aptNotes":"This Goldfish has some weird spots in the belly"},{"petName":"Mitten","petOwner":"Hillary Goldwyn","aptDate":"2017-07-21 9:15","aptNotes":"Cat has excessive hairballs"},{"petName":"Buffy","petOwner":"Hassum Harrod","aptDate":"2017-07-20 15:30","aptNotes":"This Chihuahua has not eaten for three days and is lethargic"}]
-
 export default {
   name: 'MainApp',
   data() {
     return {
       title: 'Appointments 2',
-      appointments: apts
+      theAppointments: []
     }
+  },
+  created: function() {
+    $.getJSON('/appointments.json')
+      .done( info => {
+        this.theAppointments = info;
+      })
   }
 }
 </script>
 
 <style scoped lang='sass'>
-p
-  font-size: 2em
-  text-align: center
-h1
-  background-color: green
+ul
+  background: green
 </style>
--- app/javascript/components/index.ts
+++ app/javascript/components/index.ts
@@ -1,9 +1,8 @@
-import TurbolinksAdapter from 'vue-turbolinks';
+import TurbolinksAdapter from 'vue-turbolinks'
 import Vue from 'vue/dist/vue.esm'
 import App from 'components/app.vue'
 
 Vue.use(TurbolinksAdapter)
-
 document.addEventListener('turbolinks:load', function() {
   var element = document.querySelector('#hello')
   if (element != undefined) {
